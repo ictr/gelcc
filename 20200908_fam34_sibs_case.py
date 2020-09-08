b35505154ba7a09a424c848afc2f026e7ea2b711
ClaudioPik <cwp@dartmouth.edu>
2020-09-08 16:36:05 -0400

    Update 20200908_fam34_sibs_case.py

--- 20200908_fam34_sibs_case.py
+++ 20200908_fam34_sibs_case.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+#20200908 fam34 siblings 3-34: case
 
 import vcf
 import pandas as pd
@@ -15,7 +16,7 @@ def calculate_core_count(vcf_path, output_path, phen_path):
     out_file = open(output_path, 'w')
     carrier_counts_H = ['MIC_REF_COUNT','CORE_CARRIER_COUNT','NON_CORE_CARRIER_COUNT']
     identifiers_H = ['CHROM', 'POS', 'ID', 'REF','ALT']
-##module1  fam_gts_H is list of sequenced fam102 samples
+##module1  fam_gts_H is list of sequenced fam34 samples
     fam_gts_H = ['102_101','102_112','102_121','102_124','102_132','102_136','102_137','102_180','102_301','102_311','102_313','102_346']
     headers =  identifiers_H + carrier_counts_H + fam_gts_H
     tsv_writer = csv.writer(out_file, delimiter='\t', lineterminator='\n')
@@ -26,13 +27,13 @@ def calculate_core_count(vcf_path, output_path, phen_path):
         c2s = lambda cinds: [cind2sample_name[s] for s in cinds]
         
         # observed number of married-in controls (unrelated to sibs)
-##module2  mic_ids is c2s(list of sequenced fam102 MCIs)
+##module2  mic_ids is c2s(list of sequenced fam34 MCIs)
         mic_ids = c2s(['102_346', '102_124'])
         mic_ref_count = sum(are_refs(mic_ids))
-##module3 seq_core_ids is c2s(list of sequenced fam102 LCS (affected sibs))
+##module3 seq_core_ids is c2s(list of sequenced fam34 LCS (affected sibs))
         seq_core_ids  = c2s(['102_112'])
         seq_core_carrier_count = sum(are_carriers(seq_core_ids))
-##module4: defines desc_ids for the 2 inferred core LCs in fam102
+##module4: defines desc_ids for the 2 inferred core LCs in fam34
 # number of inferred core carriers based on sequence of descendants is:  inf-core-LC = (313, 311, 301) +  (121, 132, 136, 137):
         desc111_ids = c2s(['102_313', '102_311', '102_301'])
         desc145_ids = c2s(['102_121', '102_132', '102_136', '102_137'])
@@ -47,14 +48,14 @@ def calculate_core_count(vcf_path, output_path, phen_path):
         """
         
         #observed number of sequenced non-core carriers:seq-non-core-LC = 101 + 180 
-#module6 seq_noncore_ids is c2s for sequenced non-core carriers in fam102:
+#module6 seq_noncore_ids is c2s for sequenced non-core carriers in fam34:
         seq_noncore_ids = c2s(['102_101', '102_180'])
         seq_non_core_carrier_count = sum(are_carriers(seq_noncore_ids))
         
         #Number of inferred non-core carriers:inf-non-core-LC = (136,137)
-#module7 defines desc_ids for the single inferred non-core LC in fam102: 102_123
+#module7 defines desc_ids for the single inferred non-core LC in fam34: 102_123
         desc123_ids = c2s(['102_136', '102_137'])
-#module8 inf-non-core_carrier_count for inferred non-core carriers in fam102
+#module8 inf-non-core_carrier_count for inferred non-core carriers in fam34
         inf_non_core_carrier_count = any(are_carriers(desc123_ids))
 
         non_core_carrier_count = seq_non_core_carrier_count + inf_non_core_carrier_count
@@ -81,18 +82,18 @@ def get_sample_gts(variant, ids):
 def test():
     phen_path = '20190717_Musolf_phenped.txt'
     vcf_path = '190829_for_test_data_gelcc_test1.txt'#'190826_for_test_data_gelcc.vcf'
-    output_path = 'core_counts_fam102.txt'
+    output_path = 'core_counts_fam34.txt'
     calculate_core_count(vcf_path, output_path, phen_path)
 
 if __name__ == "__main__":
     """
     examples:
     test:
-        python 20191107_calculate_core_counts_cmd_fam102_1.py test_head10.vcf 20190717_Musolf_phenped.txt 20191108_test_counts_out.txt
+        python 20191107_calculate_core_counts_cmd_fam34_1.py test_head10.vcf 20190717_Musolf_phenped.txt 20191108_test_counts_out.txt
     real:
-        python 20191107_calculate_core_counts_cmd_fam102_1.py /home/d1381v4/20191010_AGM_Musolf_fam102/20191009_Musolf_allchr_name_AF_CADD_PHRED_fixed.vcf 20190717_Musolf_phenped.txt 20191108_fam102_out.txt
+        python 20191107_calculate_core_counts_cmd_fam34_1.py /home/d1381v4/20191010_AGM_Musolf_fam34/20191009_Musolf_allchr_name_AF_CADD_PHRED_fixed.vcf 20190717_Musolf_phenped.txt 20191108_fam34_out.txt
     """
-    parser = argparse.ArgumentParser(description='Run core carrier analysis on fam102.')
+    parser = argparse.ArgumentParser(description='Run core carrier analysis on fam34.')
     parser.add_argument('vcf_path', action="store",help='Path to VCF file algotithm is to be run on')
     parser.add_argument('phen_path', action="store", help='Path to phenotype file.')
     parser.add_argument('output_path', action="store", help='Path that results are written to.')
